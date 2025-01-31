58864bab3256abe644720a6b60f1fb2f5fdc5f49
gabustos756 <gabrielbustos2@hotmail.com>
2025-01-31 16:08:27 -0300

    fix on response type

--- user-management-app/src/app/services/user.service.ts
+++ user-management-app/src/app/services/user.service.ts
@@ -3,6 +3,10 @@ import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, map } from 'rxjs';
 import { User } from '../models/user.model';
 
+interface UserResponse {
+  data: User[];
+}
+
 @Injectable({
   providedIn: 'root',
 })
@@ -14,8 +18,8 @@ export class UserService {
   constructor(private http: HttpClient) {}
 
   getUsers(): Observable<User[]> {
-    return this.http.get<any>(this.apiUrl).pipe(
-      map((response: any) => {
+    return this.http.get<UserResponse>(this.apiUrl).pipe(
+      map((response: UserResponse) => {
         const users = response.data;
         this.users.next(users);
         return users;
